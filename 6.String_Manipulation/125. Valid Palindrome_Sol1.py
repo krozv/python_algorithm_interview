bfb45ee9a1d59bf3be79b56397b125233bd116ae
krozv <krozv.code@gmail.com>
2024-01-16 17:57:50 +0900

    modify file

--- 6.String_Manipulation/125. Valid Palindrome_Sol1.py
+++ 6.String_Manipulation/125. Valid Palindrome_Sol1.py
@@ -1,14 +1,20 @@
-def isPalindrome(self, s: str) -> bool:
-    strs = []
-    s = 'A man, a plan, a canal: Panama'
-    for char in s:
-        # isalnum() - method returns True if all the characters
-        # are alphanumeric, meaning alphabet letter and numbers(0-9)
-        if char.isalnum():
-            strs.append(char.lower())
+class IsPalindrome:
+    def isPalindrome(self, s: str) -> bool:
+        strs = []
 
-        while len(strs) > 1:
-            if strs.pop(0) != strs.pop():
-                return False
+        for char in s:
+            # isalnum() - method returns True if all the characters
+            # are alphanumeric, meaning alphabet letter and numbers(0-9)
+            if char.isalnum():
+                strs.append(char.lower())
 
-        return True
+            while len(strs) > 1:
+                if strs.pop(0) != strs.pop():
+                    return False
+
+            return True
+
+
+a = IsPalindrome()
+s = 'A man, a plan, a canal: Panama'
+print(a.isPalindrome(s))
