43e3f62564d4e093312cddd962b53bfd3bff94a1
krozv <krozv.code@gmail.com>
2024-01-16 16:52:18 +0900

    Upload Valid_Palindrome_Sol1

--- 6.String_Manipulation/125. Valid Palindrome_Sol1.py
+++ 6.String_Manipulation/125. Valid Palindrome_Sol1.py
@@ -1,2 +1,14 @@
-str = []
-for char in s:
+def isPalindrome(self, s: str) -> bool:
+    strs = []
+    s = 'A man, a plan, a canal: Panama'
+    for char in s:
+        # isalnum() - method returns True if all the characters
+        # are alphanumeric, meaning alphabet letter and numbers(0-9)
+        if char.isalnum():
+            strs.append(char.lower())
+
+        while len(strs) > 1:
+            if strs.pop(0) != strs.pop():
+                return False
+
+        return True
